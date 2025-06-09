e021c7ce07960a53e9f401f5d57a3deebc724c0d
tobias.olea <tobiolea97@gmail.com>
2025-06-09 12:47:20 -0300

    Home page content

--- src/client/views/home/Home.jsx
+++ src/client/views/home/Home.jsx
@@ -1,9 +1,13 @@
+import './styles.css';
+
 function Home() {
   return (
     <div className="home-container">
-      <h1>JavaScript+React Nivel 2 | BBVA</h1>
-      <h2>Tobias Olea</h2>
-      <p>TP Final</p>
+      <div className="home-content-box">
+        <h1>JavaScript+React Nivel 2 | BBVA</h1>
+        <h2>Tobias Olea</h2>
+        <p>TP Final</p>
+      </div>
     </div>
   );
 }
